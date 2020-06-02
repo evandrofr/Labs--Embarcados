166aa6b8d614e9cd9a53a7f95efd5532798f6297
Evandro Romeiro <38050761+evandrofr@users.noreply.github.com>
2020-06-01 22:41:14 -0300

    Mudando rede

--- Lab0527/WIFI-WINC1500-get-RTOS-EXT1/WIFI-RTOS-get/src/main.h
+++ Lab0527/WIFI-WINC1500-get-RTOS-EXT1/WIFI-RTOS-get/src/main.h
@@ -48,9 +48,9 @@ extern "C" {
 #include "driver/include/m2m_wifi.h"
 
 /** Wi-Fi Settings */
-#define MAIN_WLAN_SSID                    "toca" /**< Destination SSID */
+#define MAIN_WLAN_SSID                    "Wifi" /**< Destination SSID */
 #define MAIN_WLAN_AUTH                    M2M_WIFI_SEC_WPA_PSK /**< Security manner */
-#define MAIN_WLAN_PSK                     "toca2017" /**< Password for Destination SSID */
+#define MAIN_WLAN_PSK                     "senha" /**< Password for Destination SSID */
 
 /** Using broadcast address for simplicity. */
 #define MAIN_SERVER_PORT                    (5000)
